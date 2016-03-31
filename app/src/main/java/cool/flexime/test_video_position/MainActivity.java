725c784bde478783953eba2d256a9cc9ab84fff6
Maxime Tousignant <maxtou@hotmail.com>
2016-03-31 18:22:00 -0400

    Test pour Gyro

--- app/src/main/java/cool/flexime/test_video_position/MainActivity.java
+++ app/src/main/java/cool/flexime/test_video_position/MainActivity.java
@@ -7,19 +7,35 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.content.Context;
+
 
 public class MainActivity extends ActionBarActivity {
 
+    // Variables pour les sensors de rotation
     private boolean recordingRotations;
+    TextView textX, textY, textZ;
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialisation
+        // Initialisations pour la rotation
         recordingRotations = false;
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        textX = (TextView) findViewById(R.id.textX);
+        textY = (TextView) findViewById(R.id.textY);
+        textZ = (TextView) findViewById(R.id.textZ);
     }
 
     @Override
@@ -56,17 +72,50 @@ public class MainActivity extends ActionBarActivity {
         TextView t = (TextView) findViewById(R.id.textView);
         Button b = (Button) findViewById(R.id.rotation_button);
 
-        // Arret des sensors
-        if(recordingRotations){
-            b.setText("Record rotations");
-            t.setText("Wuba luba dub dub!");
-            recordingRotations=false;
-        }
-        else{
+        // Activation des sensors
+        if(!recordingRotations){
             b.setText("Stop recording rotations");
             t.setText("Spin motherfucka!");
+
+//            sensorManager.registerListener(gyroListener, sensor,
+//                    SensorManager.SENSOR_DELAY_NORMAL);
+
             recordingRotations=true;
+        }
+        // Arret des sensors
+        else{
+            b.setText("Record rotations");
+            t.setText("Wuba luba dub dub!");
+
+//            sensorManager.unregisterListener(gyroListener);
+
+            recordingRotations=false;
+
         }
 
     }
+
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(gyroListener, sensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(gyroListener);
+    }
+
+    public SensorEventListener gyroListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int acc) { }
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            textX.setText("GyroX : " + x + " rad/s");
+            textY.setText("GyroY : " + y + " rad/s");
+            textZ.setText("GyroZ : " + z + " rad/s");
+        }
+    };
 }
